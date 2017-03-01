0d352efe0677ed47a612d095cc3b69da06b90d43
Usman Adeel <adeel@citypolyhs.org>
2017-03-01 21:21:41 +0000

    Started

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*global TinyTurtle*/
+/*global right*/
+/*global forward*/
+/*global left*/
+/*global stamp*/
+
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
 function square(){
@@ -11,8 +17,43 @@ forward(50);
 right(90);
 forward(50);
 }
+function rectangle(){
+forward(90);
+left(90);
+forward(190);
+left(90);
+forward(90);
+left(90);
+forward(190);
+left(90);
+}
 
+function pentagon(){
+left(30);
+forward(10);
+right(60);
+forward(30);
+right(30);
+forward(30);
+}
+
+function triangle(){
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(120);
+forward(50);
+}
+function house(){
+left(60);
+right(80);
+left(60);
+right(80);
+left(90);
+}
 // Type your function call below
 
-
+rectangle();
 stamp();
+
