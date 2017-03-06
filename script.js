21e48e45918e482595a2f507964c45230248f177
Usman Adeel <adeel@citypolyhs.org>
2017-03-06 21:05:48 +0000

    updated a parameter, probalby misspelled parameter

--- script.js
+++ script.js
@@ -7,16 +7,29 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
 function square(){
-forward(50);
+forward(40);
 right(90);
-forward(50);
+forward(40);
 right(90);
-forward(50);
+forward(40);
 right(90);
-forward(50);
+forward(40);
 right(90);
-forward(50);
+forward(40);
 }
+
+function square(x){
+forward(x);
+right(90);
+forward(x);
+right(90);
+forward(x);
+right(90);
+forward(x);
+right(90);
+forward(x);
+}
+
 function rectangle(){
 forward(90);
 left(90);
@@ -28,32 +41,123 @@ forward(190);
 left(90);
 }
 
-function pentagon(){
-left(30);
-forward(10);
+function rectangle(x){
+forward(x);
+left(90);
+forward(190);
+left(90);
+forward(x);
+left(90);
+forward(190);
+left(90);
+}
+
+function hexagon(){
 right(60);
 forward(30);
-right(30);
+right(60);
 forward(30);
+right(60);
+forward(30);
+right(60);
+forward(30);
+right(60);
+forward(30);
+right(60);
+forward(30);
+right(60);
+forward(30);
+}
+
+function hexagon(x){
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+}
+
+function pentagon(){
+left(18);
+forward(30);
+left(72);
+forward(30);
+left(72);
+forward(30);
+left(72);
+forward(30);
+left(72);
+forward(30);
+}
+
+function pentagon(x){
+left(18);
+forward(x);
+left(72);
+forward(x);
+left(72);
+forward(x);
+left(72);
+forward(x);
+left(72);
+forward(x);
 }
 
 function triangle(){
-right(60);
-forward(50);
-right(60);
-forward(50);
 right(120);
-forward(50);
+forward(30);
+right(120);
+forward(30);
+right(120);
+forward(30);
 }
+
+function triangle(x){
+right(120);
+forward(x);
+right(120);
+forward(x);
+right(120);
+forward(x);
+}
+
 function house(){
-left(60);
-right(80);
-left(60);
-right(80);
-left(90);
+right(60);
+forward(30);
+right(60);
+forward(30);
+right(60);
+forward(30);
+right(90);
+forward(50);
+right(90);
+forward(30);
 }
+
+function house(x){
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(60);
+forward(x);
+right(90);
+forward(x);
+right(90);
+forward(x);
+}
+
 // Type your function call below
 
-rectangle();
+triangle(190);
 stamp();
 
